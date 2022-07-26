ead01f5286dcc1c1524e4f65cf18d33ebf39dba2
Egor Dubina <belochka20022@gmail.com>
2022-07-26 09:08:55 +0500

    Initial commit

---
+++ source/scripts/nav.js
@@ -0,0 +1,35 @@
+const menuItems = document.querySelectorAll(".navlink")
+const itemsActivity = document.querySelectorAll('.activity')
+const nextBtns = document.querySelectorAll(".next-btn")
+
+function setActiveMenuItem(activity) {
+    itemsActivity.forEach((item) => {
+        if (item.getAttribute("activity") == activity) {
+            item.classList.add("active")
+        } else {
+            item.classList.remove("active")
+        }
+    })
+    menuItems.forEach((item) => {
+        if (item.getAttribute("activity") == activity) {
+            item.classList.add("active")
+        } else {
+            item.classList.remove("active")
+        }
+    })
+}
+
+// вешаем слушатель кликер на кнопки смены экрана
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        let activity = item.getAttribute('activity')
+        setActiveMenuItem(activity)
+    })
+})
+nextBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let activity = btn.getAttribute('activity')
+        console.log(btn);
+        setActiveMenuItem(activity)
+    })
+})
